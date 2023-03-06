7170a675aaf84512861031abbbd98025040e6dff
Brandon Wu <wu302966@gmail.com>
2023-03-06 14:18:22 -0800

    insert some pics

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import cherry from "./assets/cherry.png";
+import signature from "./assets/signature.png"
+import swipe from "./assets/swipe.png"
+import sub from "./assets/subject.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div full-body>
+        <div left-border>
+          <img className = 'top-left' src={cherry} alt="react logo" />
+          <img className = 'cherry1' src={cherry} alt="react logo" />
+          <img className = 'cherry2' src={cherry} alt="react logo" />
+          <img className = 'cherry3' src={cherry} alt="react logo" />
+        </div>
+        <img className = 'sig' src = {signature} alt = "react logo" />
+        <div description>
+          <hr className = 'topline'></hr>
+          <label className = 'description'>
+          Hi! I am a freshmen studying Computer Science and Business at UC Berkeley. I love front-end development and creating full stack projects for non-profits. 
+          If I’m away from my keyboard, I like to play Badminton and go to the gym. I am a Business Analyst at Consult Your Community and Codebase at Berkeley. 
+          </label>
+          <hr className = 'bottomline'></hr>
+        </div>
+        <div headshot>
+          <img className = 'swipe' src = {swipe} alt = 'react logo'/>
+          <img className = 'subject' src = {sub} alt = 'react logo'/>
+        </div>
+      </div>
     </div>
   );
 }
