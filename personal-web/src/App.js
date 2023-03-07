b839aed178b89243800d4b8d2c20c62901565b95
Brandon Wu <wu302966@gmail.com>
2023-03-07 12:04:11 -0800

    some

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -14,15 +14,15 @@ function App() {
           <img className = 'cherry2' src={cherry} alt="react logo" />
           <img className = 'cherry3' src={cherry} alt="react logo" />
         </div>
-        <img className = 'sig' src = {signature} alt = "react logo" />
-        <div description>
-          <hr className = 'topline'></hr>
-          <label className = 'description'>
-          Hi! I am a freshmen studying Computer Science and Business at UC Berkeley. I love front-end development and creating full stack projects for non-profits. 
-          If I’m away from my keyboard, I like to play Badminton and go to the gym. I am a Business Analyst at Consult Your Community and Codebase at Berkeley. 
-          </label>
-          <hr className = 'bottomline'></hr>
-        </div>
+          <img className = 'sig' src = {signature} alt = "react logo" />
+          <div description>
+            <hr className = 'topline'></hr>
+            <label className = 'description'>
+            Hi! I am a freshmen studying Computer Science and Business at UC Berkeley. I love front-end development and creating full stack projects for non-profits. 
+            If I’m away from my keyboard, I like to play Badminton and go to the gym. I am a Business Analyst at Consult Your Community and Codebase at Berkeley. 
+            </label>
+            <hr className = 'bottomline'></hr>
+          </div>
         <div headshot>
           <img className = 'headshot' src = {me} alt = 'react logo'/>
         </div>
