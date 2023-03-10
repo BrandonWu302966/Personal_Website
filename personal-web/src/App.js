33442ef54022386dae989e83a8243b34f9e0bc07
Brandon Wu <wu302966@gmail.com>
2023-03-09 19:53:13 -0800

    almost done

--- personal-web/src/App.js
+++ personal-web/src/App.js
@@ -9,22 +9,29 @@ import roots from "./assets/roots.png";
 import branch1 from "./assets/branch2.png";
 import branch2 from "./assets/branch2-1.png";
 
-
 function App() {
   return (
     <div className="App">
-      <div full-body>
-        <div left-border>
-          <img className = 'top-left' src={cherry} alt="react logo" />
-          <img className = 'cherry1' src={cherry} alt="react logo" />
-          <img className = 'cherry2' src={cherry} alt="react logo" />
-          <img className = 'cherry3' src={cherry} alt="react logo" />
-          <img className = 'cherry4' src={cherry} alt="react logo" />
+      <div id = 'full-body'>
+        <img className = 'top-left' src={cherry} alt="react logo" />
+        <img className = 'cherry1' src={cherry} alt="react logo" />
+        <img className = 'cherry3' src={cherry} alt="react logo" />
+        <div id = "container">
+          <img className = 'top-right' src = {cherry} alt = 'react logo'/>
         </div>
         <div className = 'navbar'>
           f
           <button class="nav1" >Home</button>
-          <button class="nav2" >Projects</button>
+          <div class = 'dropdown1'>
+            <button class="nav2" >Projects</button>
+            <div class = 'dropdown-content1'>
+              <button className='a'>Benford's</button>
+              <br></br>
+              <button className='a'>BFHP</button>
+              <br></br>
+              <button className='a'>Nexus</button>
+            </div>
+          </div>
           <button class="nav3">Experiences</button>
           <button class="nav4">Contacts</button>
         </div>
@@ -37,20 +44,33 @@ function App() {
             </label>
             <hr className = 'bottomline'></hr>
           </div>
-        <div headshot>
-          <img className = 'top-right' src = {cherry} alt = 'react logo'/>
+        <div className = 'headshot'>
           <img className = 'headshot' src = {swipe} alt = 'react logo'/>
           <img className = 'headshot' src = {me} alt = 'react logo'/>
         </div>
-        <div tree>
+        
+      </div>
+      <div id = 'full-body2'>
+        <div id = 'container2'>
           <img className = 'stump' src = {stump} alt = 'react logo'/>
-          <img className = 'roots' src = {roots} alt = 'react logo'/>
-          <img className = 'branch1' src = {branch1} alt = 'react logo'/>
-          <img className = 'branch2' src = {branch2} alt = 'react logo'/>
+            <img className = 'roots' src = {roots} alt = 'react logo'/>
+            <img className = 'branch1' src = {branch1} alt = 'react logo'/>
+            <img className = 'branch2' src = {branch2} alt = 'react logo'/>
+            <img className = 'cherry5' src={cherry} alt="react logo" />
+            <img className = 'cherry6' src={cherry} alt="react logo" />
+            <img className = 'cherry7' src={cherry} alt="react logo" />
+            <img className = 'cherry8' src={cherry} alt="react logo" />
+            <img className = 'cherry9' src={cherry} alt="react logo" />
+            <img className = 'cherry10' src={cherry} alt="react logo" />
+            <img className = 'uproot' src = {roots} alt = 'react logo' />
+            <img className = 'branch3' src = {branch1} alt = 'react logo'/>
+            <img className = 'branch4' src = {branch2} alt = 'react logo'/>
+            <img className = 'cherry11' src={cherry} alt="react logo" />
+            <img className = 'cherry12' src={cherry} alt="react logo" />
+          </div>
 
         </div>
       </div>
-    </div>
   );
 }
 
